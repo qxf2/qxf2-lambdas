b4c987b5ee8109b2e02d951f2ef1d9511fa38cab
Qxf2 <mak@qxf2.com>
2020-08-16 02:32:20 +0530

    Converted method to lambda

--- daily_messages/daily_messages.py
+++ daily_messages/daily_messages.py
@@ -6,29 +6,28 @@ import boto3
 import requests
 BASE_URL = 'http://daily-messages.qxf2.com'
 QUEUE_URL = 'https://sqs.ap-south-1.amazonaws.com/285993504765/skype-sender'
-CHANNEL = 'test'
 
 def clean_message(message):
     "Clean up the message received"
-    message = message.replace("'",'-')
-    message = message.replace('"','-')
+    message = message.replace("'", '-')
+    message = message.replace('"', '-')
 
     return message
 
-def get_message():
+def get_message(endpoint):
     "Get a message for employees"
-    endpoint = '/message'
     response = requests.get(url=BASE_URL+endpoint)
 
     return clean_message(response.json()['msg'])
 
-def write_message(daily_message):
+def write_message(daily_message, channel):
     "Send a message to Skype Sender"
     sqs = boto3.client('sqs')
-    message = str({'msg':f'{daily_message}', 'channel':CHANNEL})
+    message = str({'msg':f'{daily_message}', 'channel':channel})
     sqs.send_message(QueueUrl=QUEUE_URL, MessageBody=(message))
 
-def employee_daily():
-    "Post a message about Qxf2 culture"
-    message = get_message()
-    write_message(message)
+def lambda_handler(event, context):
+    "Lambda entry point"
+    #This lambda expects an event of type {'endpoint':'/blah','channel':'blah'}
+    message = get_message(event.get('endpoint'))
+    write_message(message, event.get('channel','test'))
