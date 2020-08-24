748294df0861b4c5fe81ea98f0e8c164e15771d1
Rohit prasad <rohitprasad25061999@gmail.com>
2020-08-24 11:47:45 +0530

    Added conf file, having all configure details

---
+++ github_stats/conf.py
@@ -0,0 +1,8 @@
+"GitHub stats table config."
+GITHUB_STATS_TABLE_NAME = 'github_stats'
+
+"GitHub conf details."
+GITHUB_USER = 'qxf2'
+
+"GitHub token"
+TOKEN = "5ccae741b92d2f8a23cd14b0039639a03a0da0fb"
