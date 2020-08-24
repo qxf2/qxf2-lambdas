da224f46fad4a4041eebe38a5d949cfe75934291
Rohit prasad <rohitprasad25061999@gmail.com>
2020-08-24 11:36:05 +0530

    updated code with proper docstring

--- docker_stats/conf.py
+++ docker_stats/conf.py
@@ -1,9 +1,5 @@
-"Publicly facing streams and sub-streams configuration details."
-# Docker Hub conf details.
+"Docker Hub conf details."
 DOCKER_HUB_URL = 'https://hub.docker.com/v2/repositories/qxf2rohand/'
 
-
-"Conf file to contain DynamoDB details related to docker."
-
-# Docker Hub stats table config.
+"Docker Hub stats table config."
 DOCKER_STATS_TABLE_NAME = 'docker_stats'
