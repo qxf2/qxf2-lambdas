b9643ba8fe0c14747e282db29896c96ab0f818a1
rohit679 <rohitprasad25061999@gmail.com>
2020-09-06 19:53:41 +0530

    added some config variables

--- docker_deltas/conf.py
+++ docker_deltas/conf.py
@@ -1,8 +1,11 @@
+"""
+    This Python script consists of configuration details related to dockerhub
+"""
+
+# Docker Hub tables config.
+DOCKER_SUBSTREAMS_TABLE = 'docker_substreams'
+DOCKER_STATS_TABLE = 'docker_stats'
+DOCKER_DELTAS_TABLE = 'docker_deltas'
+
 # Docker Hub conf details.
 DOCKER_HUB_URL = 'https://hub.docker.com/v2/repositories/qxf2rohand/'
-
-# Docker Hub stats table config.
-DOCKER_STATS_TABLE_NAME = 'docker_stats'
-
-# Docker Hub deltas table config.
-DOCKER_DELTAS_TABLE_NAME = 'docker_deltas'
