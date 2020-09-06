ba8cefc668319c408405df64cead9c6094f3c83f
rohit679 <rohitprasad25061999@gmail.com>
2020-09-06 19:11:06 +0530

    Added docstring to the conf

--- docker_stats/conf.py
+++ docker_stats/conf.py
@@ -1,5 +1,4 @@
 "Docker Hub conf details."
 DOCKER_HUB_URL = 'https://hub.docker.com/v2/repositories/qxf2rohand/'
-
-"Docker Hub stats table config."
 DOCKER_STATS_TABLE_NAME = 'docker_stats'
+DOCKER_SUBSTREAM_TABLE_NAME = 'docker_substreams'
