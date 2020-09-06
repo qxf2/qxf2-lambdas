5a6578f734e221c23c41024f03da825691795872
rohit679 <rohitprasad25061999@gmail.com>
2020-09-06 19:12:01 +0530

    modified code to write substream data too

--- docker_stats/docker_stats.py
+++ docker_stats/docker_stats.py
@@ -12,17 +12,21 @@ import streams_common_functions as scf
 def store_docker_stats(url):
     "Stores today's docker pulls for all containers."
     stats_obj = []#list of dicts
+    substream_obj = []#list of dicts of substream
     current_date = datetime.datetime.now().strftime('%d-%b-%Y')
     all_data = scf.get_current_docker_data(url)
     for data in all_data['results']:
         stats_obj.append({'date':current_date,\
                     'image_name':data['name'], 'pulls':data['pull_count']})
+        substream_obj.append({'date':current_date,'image_name':data['name']})
     # Write docker stats into a DB.
-    scf.write_into_db(stats_obj, cf.DOCKER_STATS_TABLE_NAME)
+    scf.write_data_into_db(stats_obj, cf.DOCKER_STATS_TABLE_NAME)
+    return substream_obj
 
 def handler(event, context):
     "handler function is the lambda function, from where execution starts"
-    store_docker_stats(cf.DOCKER_HUB_URL)
+    substream_data = store_docker_stats(cf.DOCKER_HUB_URL)
+    scf.write_substream_into_db(substream_data,cf.DOCKER_SUBSTREAM_TABLE_NAME)
     return {
         "statusCode": 200,
         "body": json.dumps({
