c8d6b09484df87705087d22e80611f8c8144b17c
rohit679 <rohitprasad25061999@gmail.com>
2020-09-06 19:55:51 +0530

    Modified code for tackling new image conflict

--- docker_deltas/streams_common_functions.py
+++ docker_deltas/streams_common_functions.py
@@ -1,64 +1,74 @@
 """
-Module to contain functions that are common across various streams:
-- stats fetching scripts.
-- score generation scripts.
+Module to contain the DynamoDB operations related to QElo.
 """
-import sys
-import os
+import re
 import boto3
-import requests
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import conf as conf
+from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
 
-
-def get_substreams(stream):
-    "Returns all the sub-streams for the provided stream."
-    substreams = get_current_docker_data()
+def extract_substream_names(substreams_data, key):
+    "Extracts the name of substreams from the complete substreams data."
+    substreams = [substream_data[key] for substream_data in substreams_data]
     return substreams
 
-def get_current_docker_data():
-    "Gets the current data from Docker Hub for all containers."
-    response = requests.get(conf.DOCKER_HUB_URL)
-    result = response.json()
-    return result
-
-def read_db(stream_access_info, table_name, date):
-    "Retrieves items that match the criterias, from the DynamoDB table."
+def read_substreams(table_name, date):
+    "Retrieves substreams from the corresponding DynamoDB table."
     dynamodb = None
-    stats_obj = [] #list of dicts
-    # Collect all sub-streams.
-    substreams_info = get_substreams(table_name)
     # Initialise a DynomoDB table resource.
     if not dynamodb:
         dynamodb = boto3.resource('dynamodb')
         table = dynamodb.Table(table_name)
-    if table_name in (conf.DOCKER_STATS_TABLE_NAME):
-        try:
-            for container_data in substreams_info['results']:
-                response = table.get_item(\
-                    Key={'date':date,\
-                         'image_name':container_data['name']}\
+    try:
+        filtering_exp = Key('date').eq(date)
+        response = table.query(KeyConditionExpression=filtering_exp)
+        if re.search(r'^github', table_name):
+            data = extract_substream_names(response['Items'], 'repo_name')
+        else:
+            data = extract_substream_names(response['Items'], 'image_name')
+        print(f"\n Read records for {date} successfully from DynamoDB table {table_name}.")
+        return data
+    except ClientError as dynamodb_error:
+        print(f'\n Error while reading from table {table_name} : \n {dynamodb_error.response}')
+        raise Exception('Exception encountered and run aborted!.')
+    except Exception:
+        raise Exception('Exception while reading data from table.')
+
+def read_docker_table(table_name, date, substreams):
+    "Retrieves items that match the criterias, from corresponding Docker DynamoDB table."
+    dynamodb = None
+    data = [] #list of dicts
+    # Initialise a DynomoDB table resource.
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(table_name)
+    try:
+        for substream in substreams:
+            response = table.get_item(
+                Key={\
+                        'date':date,\
+                        'image_name':substream\
+                    }\
                 )
-                stats_obj.append(response['Item'])
-        except Exception:
-            raise Exception('Exception while reading data from table.')
-    return stats_obj
+            data.append(response['Item'])
+        print(f"\n Read records for {date} successfully from DynamoDB table {table_name}.")
+    except Exception:
+        raise Exception('Exception while reading data from table.')
+    return data
 
-def write_into_db(info, table_name):
-    "Writes info into DynamoDB table."
+def write_into_table(items, table_name):
+    "Writes items/records into DynamoDB table."
     dynamodb = None
     # Initialise a DynomoDB table resource.
     if not dynamodb:
         dynamodb = boto3.resource('dynamodb')
         table = dynamodb.Table(table_name)
-    if table_name in (conf.DOCKER_DELTAS_TABLE_NAME):
-        try:
-            with table.batch_writer() as batch:
-                for container_stats in info:
-                    date = container_stats['date']
-                    container_name = container_stats['image_name']
-                    print("Adding record for {} on {} to DynamoDB table {}."\
-                            .format(container_name, date, table_name))
-                    batch.put_item(Item=container_stats)
-        except Exception:
-            raise Exception('Exception while inserting data into table.')
+    try:
+        with table.batch_writer() as batch:
+            for item in items:
+                batch.put_item(Item=item)
+        print(f"\n Added today's records successfully into DynamoDB table {table_name}.")
+    except ClientError as dynamodb_error:
+        print(f'\n Error while writing into table {table_name} : \n {dynamodb_error.response}')
+        raise Exception('Exception encountered and run aborted!.')
+    except Exception:
+        raise Exception('Exception while inserting data into table.')
