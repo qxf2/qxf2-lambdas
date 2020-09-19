f70f63da2b8bf27717988b282d7b2a7c2ec99139
rohit679 <rohitprasad25061999@gmail.com>
2020-09-19 19:03:14 +0530

    merged streams_common_functions code into it

--- docker_stats/docker_stats.py
+++ docker_stats/docker_stats.py
@@ -5,30 +5,56 @@ Collect and store the Docker Hub stats for all Qxf2 docker containers.
 This script is expected to run every night.
 """
 import json
+import boto3
 import datetime
+import requests
 import conf
-import streams_common_functions as common_func
 
-def store_docker_stats(url):
-    "Stores today's docker pulls for all containers."
-    stats_obj = []#list of dicts
-    substream_obj = []#list of dicts of substream
+def write_into_table(items, table_name):
+    "Writes items/records into DynamoDB table."
+    dynamodb = None
+    # Initialise a DynomoDB table resource.
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(table_name)
+    try:
+        with table.batch_writer() as batch:
+            for item in items:
+                batch.put_item(Item=item)
+        print(f"Added today's records successfully into DynamoDB table {table_name}.")
+        return 'data collected in {} table'.format(table_name)
+    except Exception:
+        return 'Exception while inserting data into {} table'.format(table_name)
+
+def prepare_data(all_data):
+    "Prepare data in proper form to collect into dynamodb table"
+    stats,substreams,result = [],[],[]
     current_date = datetime.datetime.now().strftime('%d-%b-%Y')
-    all_data = common_func.get_current_docker_data(url)
-    for data in all_data['results']:
-        stats_obj.append({'date':current_date,\
+    for image in all_data:
+        substreams.append({'date':current_date,\
+                    'image_name':image['name']})
+    result.append(substreams)
+    for data in all_data:
+        stats.append({'date':current_date,\
                     'image_name':data['name'], 'pulls':data['pull_count']})
-        substream_obj.append({'date':current_date,'image_name':data['name']})
-    # Write docker stats into a DB.
-    common_func.write_data_into_db(stats_obj, conf.DOCKER_STATS_TABLE_NAME)
-    return substream_obj
+    result.append(stats)
+    return result
+
+def get_docker_image_data():
+    "Gets the data from Docker Hub for all docker images."
+    response = requests.get(conf.DOCKER_HUB_URL)
+    result = response.json()
+    return result['results']
 
 def handler(event, context):
     "handler function is the lambda function, from where execution starts"
-    substream_data = store_docker_stats(conf.DOCKER_HUB_URL)
-    common_func.write_substream_into_db(substream_data,conf.DOCKER_SUBSTREAM_TABLE_NAME)
-    return {
-        "body": json.dumps({
-            "message": "docker_stats running properly",
-        }),
-    }
+    all_data = get_docker_image_data()
+    print("current docker data collected:)")
+    substreams_data, stats_data = prepare_data(all_data)
+    print("data prepared for writing!!!")
+    message1 = write_into_table(substreams_data, conf.DOCKER_SUBSTREAMS_TABLE)
+    message2 = write_into_table(stats_data, conf.DOCKER_STATS_TABLE)
+    if message1 == 'data collected in docker_substreams table' and message2 == 'data collected in docker_stats table':
+        return "docker stats collected successfully"
+    else:
+        return "Error while writing into dynamodb table"
