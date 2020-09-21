cf8c76cc9f3d53b7c2991124aafc15337a7b1eb6
rohit679 <rohitprasad25061999@gmail.com>
2020-09-21 16:49:28 +0530

    new changes

--- github_stats/conf.py
+++ github_stats/conf.py
@@ -4,5 +4,4 @@ import os
 GITHUB_STATS_TABLE = 'github_stats'
 GITHUB_USER = 'qxf2'
 GITHUB_SUBSTREAMS_TABLE = 'github_substreams'
-# TOKEN = os.environ["ACCESS_KEY"]
-TOKEN = "93d7260d75e4ce35ae2e4f030b70ca7a6fbd8db5"
+TOKEN = os.environ["ACCESS_KEY"]
