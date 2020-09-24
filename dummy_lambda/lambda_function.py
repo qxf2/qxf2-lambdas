e9443a2ddf9b6cadc2ad19f63c62f41684c13c5c
Qxf2 <mak@qxf2.com>
2020-09-24 23:47:37 +0530

    Added a trailing slash

--- dummy_lambda/lambda_function.py
+++ dummy_lambda/lambda_function.py
@@ -3,6 +3,7 @@ Simplest lambda to play with GitHub actions
 Try 1: Failed. Simply change requirements.txt path
 Try 2: Changing the action to include a directory
 Try 3: Added a tag
+Try 4: Added a trailing slash
 """
 import json
 
