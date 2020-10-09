0075853d7ea815aa37b1298065660f5e70fb8b1f
rohit679 <rohitprasad25061999@gmail.com>
2020-10-09 15:59:11 +0530

    removed previous codes

--- docker_stats/conf.py
+++
@@ -1,4 +0,0 @@
-"Docker Hub conf details."
-DOCKER_HUB_URL = 'https://hub.docker.com/v2/repositories/qxf2rohand/'
-DOCKER_STATS_TABLE = 'docker_stats'
-DOCKER_SUBSTREAMS_TABLE = 'docker_substreams'
--- github_stats/conf.py
+++
@@ -1,7 +0,0 @@
-"GitHub config. details"
-import os
-
-GITHUB_STATS_TABLE = 'github_stats'
-GITHUB_USER = 'qxf2'
-GITHUB_SUBSTREAMS_TABLE = 'github_substreams'
-TOKEN = os.environ["ACCESS_KEY"]
