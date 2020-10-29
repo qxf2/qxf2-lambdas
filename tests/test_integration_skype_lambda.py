6a598b6b7ed9120d90cac677faa4be013862ca65
rahulbhave <55383777+rahul-bhave@users.noreply.github.com>
2020-10-29 14:41:44 +0530

    first-draft (#9)
    review comments
    
    tests/test_integration_skype_lambda.py
    
    fixed coverage report in daily message lambda
    
    fix deprecation,updated requirement and readme

---
+++ tests/test_integration_skype_lambda.py
@@ -0,0 +1,25 @@
+"""
+Code level tests for the skype sender lambda
+"""
+import os
+import sys
+import boto3
+from unittest import mock
+from unittest.mock import patch
+from skype_sender import qxf2_skype_sender
+import conf.channel_conf as conf
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+@patch('skype_sender.qxf2_skype_sender.post_message')
+def test_skype_sender_lambda(mock_post_message):
+    "Test to write post messages"
+    event = {"Records": [{"body": "{'msg': '<b>Test message</b> I am a test message from lambda-tester', 'channel':conf.channel}"}]}
+    mock_post_message.return_value = '<b>Test message</b> I am a test message from lambda-tester'
+
+    # Triggering the response
+    qxf2_skype_sender.post_message(event=event,context=None)
+
+    #Assertion
+    mock_post_message.assert_called_with(event=event,context=None,)
+    assert mock_post_message.call_count == 1
