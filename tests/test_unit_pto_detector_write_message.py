c7611de4b66911f3f120129a65d6575e9599e340
Rahul.Bhave <rahul.bhave@qxf2.com>
2020-12-14 14:06:45 +0530

    mocked sqs queue for write_message
    seperate out code for get_sqs_message
    
    parameterize test for write_message

--- tests/test_unit_pto_detector_write_message.py
+++ tests/test_unit_pto_detector_write_message.py
@@ -1,10 +1,11 @@
-import os, sys
+import os,sys
 import boto3
 import conf.channel_conf as conf
 import warnings
+import unittest
 from unittest import mock
-from unittest.mock import patch
 from moto import mock_sqs
+from parameterized import parameterized_class
 from pto_detector import pto_detector
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -13,20 +14,21 @@ with warnings.catch_warnings():
     warnings.filterwarnings("ignore",category=DeprecationWarning)
     import imp
 
+@parameterized_class(("message_to_send", "expected_message"), [
+   ('Detected PTO message Test: I am out sick today',str({'msg':f'Detected PTO message Test: I am out sick today', 'channel':conf.channel}),'Detected PTO message Test: I am on PTO tomorrow',str({'msg':f'Detected PTO message Test: I am on PTO tomorrow', 'channel':conf.channel}))])
+
 @mock_sqs
-@patch('pto_detector.pto_detector.write_message')
-def test_pto_detector_write_message(mock_write_message):
-    """
-    Unit test message for write_messsage
-    """
-    mock_write_message.return_value('Detected PTO message Test: I am out sick today')
-    expected_message = str({'msg':f'Detected PTO message Test: I am out sick today', 'channel':conf.channel})
-    message_to_send = 'Detected PTO message Test: I am out sick today'
-    channel = 'test'
-    sqs = boto3.resource('sqs')
-    queue = sqs.create_queue(QueueName='test-skype-sender')
-    pto_detector.QUEUE_URL = queue.url
-    pto_detector.write_message(message_to_send,channel)
-    sqs_message = queue.receive_messages()
-    mock_write_message.called_with('Detected PTO message Test: I am out sick today',conf.channel)
-    assert mock_write_message.call_count == 1
+class TestWriteMessage(unittest.TestCase):
+    "Declaring class for write method"
+
+    def test_pto_detector_write_message(self):
+        """
+        Unit test message for write_messsage
+        """
+        sqs = boto3.resource('sqs')
+        queue = sqs.create_queue(QueueName='test-skype-sender')
+        pto_detector.QUEUE_URL = queue.url
+        pto_detector.write_message(self.message_to_send,conf.channel)
+        sqs_message = queue.receive_messages()
+        self.assertEqual(sqs_message[0].body,self.expected_message)
+        assert len(sqs_message) == 1
