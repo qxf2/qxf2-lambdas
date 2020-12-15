a7cb2f4ff87299372e50a322f59910093b80fb6d
Rahul.Bhave <rahul.bhave@qxf2.com>
2020-12-15 11:55:20 +0530

    added unit test main method
    Added get_class_name method to include multiple parameters

--- tests/test_unit_pto_detector_write_message.py
+++ tests/test_unit_pto_detector_write_message.py
@@ -5,7 +5,7 @@ import warnings
 import unittest
 from unittest import mock
 from moto import mock_sqs
-from parameterized import parameterized_class
+from parameterized import parameterized, parameterized_class
 from pto_detector import pto_detector
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -14,8 +14,21 @@ with warnings.catch_warnings():
     warnings.filterwarnings("ignore",category=DeprecationWarning)
     import imp
 
-@parameterized_class(("message_to_send", "expected_message"), [
-   ('Detected PTO message Test: I am out sick today',str({'msg':f'Detected PTO message Test: I am out sick today', 'channel':conf.channel}),'Detected PTO message Test: I am on PTO tomorrow',str({'msg':f'Detected PTO message Test: I am on PTO tomorrow', 'channel':conf.channel}))])
+def get_class_name(cls, num, params_dict):
+    """
+    multiple parameters being included in the generated class name:
+    """
+    return "%s_%s_%s%s" %(
+        cls.__name__,
+        num,
+        parameterized.to_safe_name(params_dict['message_to_send']),
+        parameterized.to_safe_name(params_dict['expected_message']),
+    )
+
+@parameterized_class([
+   { "message_to_send": "Detected PTO message Test: I am out sick today", "expected_message": str({'msg':f'Detected PTO message Test: I am out sick today', 'channel':conf.channel})},
+   { "message_to_send": "Detected PTO message Test: I am on PTO today", "expected_message": str({'msg':f'Detected PTO message Test: I am on PTO today', 'channel':conf.channel})},
+], class_name_func=get_class_name)
 
 @mock_sqs
 class TestWriteMessage(unittest.TestCase):
@@ -31,4 +44,8 @@ class TestWriteMessage(unittest.TestCase):
         pto_detector.write_message(self.message_to_send,conf.channel)
         sqs_message = queue.receive_messages()
         self.assertEqual(sqs_message[0].body,self.expected_message)
-        assert len(sqs_message) == 1
+        #assert len(sqs_message) == 1
+        self.assertEqual(len(sqs_message),1)
+
+if __name__ == "__main__":
+    unittest.main()
