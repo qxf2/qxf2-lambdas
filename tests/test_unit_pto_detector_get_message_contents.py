3ad6f0196ed233c0decb58358fff12a0808cbcf9
Rahul.Bhave <rahul.bhave@qxf2.com>
2020-12-15 19:42:35 +0530

    removed patch
    Implemented parameterize class

--- tests/test_unit_pto_detector_get_message_contents.py
+++ tests/test_unit_pto_detector_get_message_contents.py
@@ -1,28 +1,65 @@
-import os, sys
+import ast
+import collections
+import os
+import sys
 import boto3
+import json
 import warnings
-from unittest import mock
-from unittest.mock import patch
-from moto import mock_sqs
+import unittest
+from parameterized import parameterized, parameterized_class
 from pto_detector import pto_detector
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-@mock_sqs
-@patch('pto_detector.pto_detector.get_message_contents')
-def test_pto_detector_get_message_contents(mock_get_message_contents):
+def get_class_name(cls, num, params_dict):
     """
-    Unit test for pto_detctor
+    multiple parameters being included in the generated class name:
     """
-    event = {
-        "Records": [
-            {
-            "body": "{\"Message\":\"{\\\"msg\\\": \\\"Test: I am out sick today\\\", \\\"chat_id\\\": \\\"19:f33e901e871d4c3c9ebbbbee66e59ebe@thread.skype\\\", \\\"user_id\\\":\\\"blah\\\"}\"}"
-            }
-        ]
-    }
+    return "%s_%s_%s%s" %(
+        cls.__name__,
+        num,
+        parameterized.to_safe_name(params_dict['event']),
+        parameterized.to_safe_name(params_dict['expected_message_contents']),
+    )
 
-    mock_get_message_contents.return_value =('Test: I am out sick today', 'blah',  '19:f33e901e871d4c3c9ebbbbee66e59ebe@thread.skype')
-    expected_message_contents = str({'msg': 'Test: I am out sick today', 'user_id': 'blah', 'chat_id': '19:f33e901e871d4c3c9ebbbbee66e59ebe@thread.skype'})
-    pto_detector.get_message_contents(event)
-    assert mock_get_message_contents.call_count == 1
-    mock_get_message_contents.called_with('Test: I am out sick today', 'blah',  '19:f33e901e871d4c3c9ebbbbee66e59ebe@thread.skype')
+@parameterized_class([
+   { "event":str({"Records": [{"body": "{\"Message\":\"{\\\"msg\\\": \\\"Test: I am on PTO today\\\", \\\"chat_id\\\": \\\"19:f33e901e871d4c3c9ebbbbee66e59ebe@thread.skype\\\", \\\"user_id\\\":\\\"blah\\\"}\"}"}]}), "expected_message_contents": str({'msg': 'Test: I am on PTO today', 'chat_id': '19:f33e901e871d4c3c9ebbbbee66e59ebe@thread.skype','user_id': 'blah'})}], class_name_func=get_class_name)
+
+class TestWriteMessage(unittest.TestCase):
+    """
+    Test class for get message contents
+    """
+    def get_data_structure(self, data):
+        """
+        Method used for converting nested dictionary/list to data similar to tabular form
+        """
+        obj = collections.OrderedDict()
+        def recurse(dataobject,parent_key=""):
+            """
+            Method will recurse through object
+            """
+            if isinstance(dataobject,list):
+                # loop through list and call recurse()
+                for i in range(len(dataobject)):
+                    recurse(dataobject[i],parent_key + "_" + str(i) if parent_key else str(i))
+            elif isinstance(dataobject,dict):
+                # loop through dictionary and call recurse()
+                for key,value in dataobject.items():
+                    recurse(value,parent_key + "_" + key if parent_key else key)
+            else:
+                # use the parent_key and store the value to obj
+                obj[parent_key] = dataobject
+
+        recurse(data)
+
+        return obj
+
+    def test_pto_detector_get_message_contents(self):
+        """
+        Unit test for pto_detctor
+        """
+        event_structure = self.get_data_structure(self.event)
+        event_dict =ast.literal_eval(event_structure[''])
+        actual_message = pto_detector.get_message_contents(event_dict)
+        self.assertEqual(str(actual_message), self.expected_message_contents)
+
+if __name__ == "__main__":
+    unittest.main()
