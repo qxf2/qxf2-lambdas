df664cd064af86afea598a05ba45c0446a01d73e
Rahul.Bhave <rahul.bhave@qxf2.com>
2020-12-16 21:11:02 +0530

    removed responses module

--- tests/test_unit_pto_detector_get_is_pto.py
+++ tests/test_unit_pto_detector_get_is_pto.py
@@ -1,29 +1,21 @@
-import responses
 import requests
 import unittest
 from parameterized import parameterized, parameterized_class
 
 @parameterized_class(("message", "score", "expected_status_code"), [
    ("I am on PTO today", 1, 200),
+   ("I am happy today", 0, 200),
 ])
 
 class Testgetispto(unittest.TestCase):
     """
     Test class for get is pto method
     """
-    @responses.activate
+    #@responses.activate
     def test_unit_get_is_pto_score(self):
         """
         Unit test for pto_score
         """
-        responses.add(
-            responses.POST,
-            url='https://practice-testing-ai-ml.qxf2.com/is-pto',
-            body='{"message":"I am on PTO today","score":1}',
-            match=[
-                responses.urlencoded_params_matcher({"message":self.message})
-            ]
-        )
         resp = requests.post("https://practice-testing-ai-ml.qxf2.com/is-pto", data={"message":self.message})
         self.assertEqual(resp.status_code,self.expected_status_code)
         self.assertEqual(resp.json()['score'],self.score)
