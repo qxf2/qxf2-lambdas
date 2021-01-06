f3dfc5c8f629d4e43acf31aaca44d9ac2924d9a2
PreedhiVivek <preedhi.vivek@qxf2.com>
2021-01-06 12:11:58 +0530

    Lambda function to store the GitHub substreams scores into DynamoDB every night.

---
+++ qelo/store_github_scores/github_conf.py
@@ -0,0 +1,4 @@
+"Configuration details realted to GitHub stream of QElo."
+GITHUB_SUBSTREAMS_TABLE = 'github_substreams'
+GITHUB_DELTAS_TABLE = 'github_deltas'
+GITHUB_SCORES_TABLE = 'github_scores'
