5410ba5aae338331c996dd7fff28a3c82230df4d
PreedhiVivek <preedhi.vivek@qxf2.com>
2021-01-07 13:25:06 +0530

    Optimised code and updated module docstring.

--- qelo/store_docker_scores/docker_conf.py
+++ qelo/store_docker_scores/docker_conf.py
@@ -1,7 +1,4 @@
-"Configuration details realted to Docker Hub stream of QElo."
-# Docker Hub URL.
-DOCKER_HUB_URL = 'https://hub.docker.com/v2/repositories/qxf2rohand/'
-
+"Module contains configuration details realted to Docker Hub stream of QElo."
 # DynamoDB tables that store Docker Hub related data.
 DOCKER_SUBSTREAMS_TABLE = 'docker_substreams'
 DOCKER_DELTAS_TABLE = 'docker_deltas'
