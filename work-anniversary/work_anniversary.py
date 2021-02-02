34a2900222bb2925ee0d915c8f96e3f589057fc5
Rohan Joshi <rohan.j@qxf2.com>
2021-02-02 09:13:45 +0530

    Removed unwanted code and hardcoded data

--- work-anniversary/work_anniversary.py
+++ work-anniversary/work_anniversary.py
@@ -1,3 +1,7 @@
+"""
+Get employees joining date and post work anniversary image to skype 
+"""
+
 import datetime
 from datetime import date
 from dateutil.relativedelta import relativedelta
@@ -14,13 +18,6 @@ SKYPE_URL = 'https://skype-sender.qxf2.com/send-image'
 BASE_URL = 'https://qxf2-employees.qxf2.com/graphql'
 inflect_obj = inflect.engine()
 
-emp_data = [{'node': {'email': 'abc@qxf2.com', 'firstname': 'Rohan', 'lastname': 'Joshi', 'dateJoined': '28-Jan-20', 'isActive': 'Y'}},
-{'node': {'email': 'xyz@qxf2.com', 'firstname': 'Rohan', 'lastname': 'Dudam', 'dateJoined': '29-Jan-19', 'isActive': 'Y'}},
-{'node': {'email': 'pqr@qxf2.com', 'firstname': 'Shivahari', 'lastname': 'Pitchaikkannu', 'dateJoined': '29-Jan-14', 'isActive': 'Y'}},
-{'node': {'email': 'def@qxf2.com', 'firstname': 'Avinash', 'lastname': 'Shetty', 'dateJoined': '29-Jan-15', 'isActive': 'Y'}},
- {'node': {'email': 'rst@qxf2.com', 'firstname': 'rs', 'lastname': 't', 'dateJoined': '29-Jan-16', 'isActive': 'N'}}]
-
-
 emp_list = []
 def authenticate():
     "Return an authenticate code"
@@ -32,7 +29,6 @@ def authenticate():
         }}
     """
     response = requests.post(url = BASE_URL, json = {'query': query})
-
     return response.json().get('data',{}).get('auth',{}).get('accessToken',None)
 
 def get_all_employees():
@@ -64,7 +60,6 @@ def is_active_employee(each_node):
     result_flag = True if each_node['node']['isActive'] == 'Y' else False
     return result_flag
 
-
 def calculate_work_anniversary(emp_joined_date, current_date, emp_name):
     "Calculate the difference"
     msg,quote_string = None,None
@@ -80,7 +75,7 @@ def add_text_to_image(message,emp_name,quote_string):
     image = Image.open('Work_anniversary_template.png')
     draw = ImageDraw.Draw(image)
     font1 = ImageFont.truetype('Casanova_Font_Free.ttf', size=120)
-    font2 = ImageFont.truetype('Casanova_Font_Free.ttf', size=70)
+    font2 = ImageFont.truetype('Casanova_Font_Free.ttf', size=75)
 
     # starting position of the message
     (x, y) = (650, 700)
@@ -108,7 +103,7 @@ def send_image(img, img_name, channel_id = credentials.CHANNEL_ID):
 
 def is_work_anniversary():
     "Get the work anniversary"
-    #emp_data = get_all_employees()
+    emp_data = get_all_employees()
     for each_node in emp_data:
         employee_active = is_active_employee(each_node)
         emp_joined_date = each_node['node']['dateJoined']
