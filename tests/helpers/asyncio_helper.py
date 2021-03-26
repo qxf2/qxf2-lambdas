b14af3bf1c67ab7afb8a52ad056534ed83f4fe44
rahulbhave <55383777+rahul-bhave@users.noreply.github.com>
2021-03-26 15:07:07 +0530

    removed sleep from main (#56)
    Message from cloudwatch pulled
    
    removed unnecessary comments
    
    added code for sqs message checks
    
    fixed issues in filter messages
    
    added pytest exit
    
    pytest exit put message as passed
    
    added exit code in the publish compare result
    
    added conftest
    
    updated pytest exit in validate message from sqs queue
    
    fixed issue with asynchronous validations
    
    fixed pylint issues
    
    updated culture file and skype sender yml file
    
    review comment fixes
    
    updated pytest exemption
    
    review comments
    
    review comments
    
    updated yml to include test name
    
    updated print statement in conftest
    
    added time in test results
    
    updated branch to master in yml file
    
    updated test name in the path

--- tests/helpers/asyncio_helper.py
+++ tests/helpers/asyncio_helper.py
@@ -1,11 +1,28 @@
 """
 Helper module for asyncio methods
 """
+import sys
 import asyncio
 import logging
-import helpers.filter_message_helper
-import helpers.sqs_helper
-import conf.sqs_utilities_conf as queue_url_conf
+import tests.helpers.filter_message_helper
+import tests.helpers.sqs_helper
+import tests.conf.sqs_utilities_conf as queue_url_conf
+
+# Declaring class Style
+class Style():
+    """
+    Declaring Style class
+    """
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
 
 async def validate_message_with_sqs(queue_url, message_cloudwatch):
     """
@@ -15,8 +32,11 @@ async def validate_message_with_sqs(queue_url, message_cloudwatch):
     """
     _logger = logging.getLogger(__name__)
     _logger.setLevel(logging.DEBUG)
-    message = helpers.sqs_helper.get_message_from_queue(queue_url)
-    result_flag = helpers.filter_message_helper.publish_compare_result(message,message_cloudwatch)
+    message = tests.helpers.sqs_helper.get_message_from_queue(queue_url)
+    result_flag = tests.helpers.filter_message_helper.publish_compare_result\
+        (message,message_cloudwatch)
+    if result_flag is True:
+        sys.exit()
 
     return result_flag
 
