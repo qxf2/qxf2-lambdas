95440e1bd5fae8ef9c754906ab6b80705c9c5097
Annapoorani Bala <Arunkumar>
2021-04-09 16:21:09 +0530

    changed the test name

--- tests/test_contract_work_service.py
+++ tests/test_contract_work_service.py
@@ -9,7 +9,6 @@ import requests
 import logging
 from pact import Consumer, Provider, Format
 
-
 # Declaring logger
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -19,26 +18,28 @@ print(Format().__dict__)
 REPO_DIR = os.path.dirname(os.path.dirname(__file__))
 PACT_DIR = os.path.join(REPO_DIR,'tests')
 
-pact = Consumer('Qxf2 Employee Service').has_pact_with(Provider('Qxf2 daily messages microservices'),pact_dir=PACT_DIR, log_dir=PACT_DIR)
+
+# Setting up pact
+pact = Consumer('Consumer').has_pact_with(Provider('Provider'))
 pact.start_service()
 atexit.register(pact.stop_service)
 
+print(pact)
 class Workanniversary(unittest.TestCase):
     "Contract test"
-    def check_employee_data_return(self):
+    def test_check_employee_data_return(self):
         "check it's returning the correct employee data"
         expected = {
         'msg' : '{"errors":[{"message":"Must provide query string."}]}'
         }
-
         (pact\
         . given('Request to get home page') \
         . upon_receiving('a request for home page')\
-        . with_request('GET', '/')\
+        . with_request('GET', '/qxf2-employees.qxf2.com/graphql')\
         . will_respond_with(200, body=expected))\
 
         with pact:\
-            result = requests.get(pact.uri + '/')\
+            result = requests.get(pact.uri + '/qxf2-employees.qxf2.com/graphql')\
 
         self.assertEqual(result.json(), expected)
         pact.verify()
