8170079f27c600abcafcb7e1835d411e37e01d80
Rohini Gopal <rohini.gopal@qxf2.com>
2021-04-09 15:59:56 +0530

    added method to send message to sqs continously

---
+++ tests/test_reslience.py
@@ -0,0 +1,27 @@
+"""
+This is relience testing to test the impact of sending
+tones of messages to the listener
+
+"""
+
+import os
+import sys
+import time
+import boto3
+
+sqs = boto3.client('sqs')
+
+def send_messages_to_sqs():
+    "Send messages continuously"
+    for range (20):
+        queue_url = 'aws:lambda:ap-south-1:285993504765:function:pto_detector'
+        response = sqs.send_message(
+        QueueUrl=queue_url,
+        DelaySeconds=10,
+        MessageAttributes={
+            MessageBody=(
+                'Information about current NY Times fiction bestseller for '
+                'week of 12/11/2016.'
+            )
+        )
+        print(response['MessageId'])
