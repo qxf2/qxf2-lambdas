b1c9e6e71cfdb1ba66de9c82bb91f97acdb7c253
Drishya <drishya.tm@qxf2.com>
2021-04-09 16:21:25 +0530

    corrected the typos

---
+++ tests/test_unit_work_anniversary_is_active_employee.py
@@ -0,0 +1,16 @@
+"""
+Unit tests for is_active method of work_anniversary
+"""
+import unittest, mock
+import work_anniversary
+
+class Test_work anniversary(unittest.TestCase):
+    """
+    Test class for active employee
+    """
+    
+    def test_unit_is_active_employee(each_node):
+        "Check the employee is active or not"
+        each_node = {'node':{'isActive':'Y'}}
+        result_flag = work_anniversary.is_active_employee(each_node)
+        assert result_flag is True
