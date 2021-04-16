068e4d2adb837034e35bfee23ffac181b9adf028
Drishya <drishya.tm@qxf2.com>
2021-04-16 21:01:03 +0530

    corrected typo of the filename

--- tests/test_unit_work_anniversary_is_active_employee.py
+++ tests/test_unit_work_anniversary_is_active_employee.py
@@ -6,11 +6,13 @@ import work_anniversary
 
 class Test_work anniversary(unittest.TestCase):
     """
-    Test class for active employee
+    Test class for work anniversary
     """
     
     def test_unit_is_active_employee(each_node):
-        "Check the employee is active or not"
+        "Test the employee is active or not"
         each_node = {'node':{'isActive':'Y'}}
         result_flag = work_anniversary.is_active_employee(each_node)
         assert result_flag is True
+        
+    
