e9c922d228d07400d27672016f3a242948e1f5ac
Sravanti <sravanti.tatiraju@qxf2.com>
2021-05-25 08:56:57 +0530

    Add lambda for notifying colleagues on paricipant join

---
+++ raspberry_pi/callout_colleague.py
@@ -0,0 +1,2 @@
+import os
+os.system("espeak -s 125 --stdout 'Hello, Your colleagues are on Call. Please join, if you are free' | aplay")
---
+++ raspberry_pi/credentials.py
@@ -0,0 +1,5 @@
+username = "rohan@qxf2.com"
+password = "TestQxf2"
+proxy_username = "pi"
+proxy_password = "raspberry"
+developerkey = "NzE5Rjk3OUYtNzNGQS00NjU5LUFBNzAtOUYyQjFDNkNEODA3"
