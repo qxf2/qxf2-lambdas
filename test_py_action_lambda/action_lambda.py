e6aef1c733b31ff8fd8080c6e9e481eed400b037
Sravanti <sravanti.tatiraju@qxf2.com>
2021-05-28 13:02:35 +0530

    Changed function name

---
+++ test_py_action_lambda/action_lambda.py
@@ -0,0 +1,19 @@
+"""
+Simplest lambda to play with GitHub actions
+"""
+import json
+import requests
+
+def action_handler(event, context):
+    print("This is lamda entry point")
+    response = requests.get('https://api.github.com')
+    print("The respone is ", response)
+    if response.status_code == 200:
+        print('Success!')
+    elif response.status_code == 404:
+        print('Not Found.')
+
+
+if __name__ == "__main__":
+    print("Hi, I am testing no function scenario")
+    lambda_handler("", "")
