25e3d73636b9bded8882fc5682db29fb1daef47a
Rohan Joshi <rohan.j@qxf2.com>
2021-06-14 16:10:03 +0530

    Updated survey reminder logic

--- survey_reminder/survey_reminder.py
+++ survey_reminder/survey_reminder.py
@@ -13,6 +13,7 @@ BASE_URL = 'https://qxf2-employees.qxf2.com/graphql'
 QUEUE_URL = 'https://sqs.ap-south-1.amazonaws.com/285993504765/skype-sender'
 NOT_RESPONDED_USERS_URL = os.environ.get('NOT_RESPONDED_USERS_URL')
 
+
 def authenticate():
     "Return an authenticate code"
     query = f"""mutation {{
@@ -22,8 +23,9 @@ def authenticate():
             }}
         }}
     """
-    response = requests.post(url = BASE_URL, json = {'query': query})
-    return response.json().get('data',{}).get('auth',{}).get('accessToken',None)
+    response = requests.post(url=BASE_URL, json={'query': query})
+    return response.json().get('data', {}).get('auth', {}).get('accessToken', None)
+
 
 def get_all_employees():
     "Query allEmployees"
@@ -42,30 +44,45 @@ def get_all_employees():
     }"""
     access_token = authenticate()
     headers = {'Authorization': f'Bearer {access_token}'}
-    response = requests.get(url = BASE_URL, json = {'query': query}, headers =\
-        headers)
-    all_employees = response.json().get('data', {}).get('allEmployees', {}).get('edges', [])
+    response = requests.get(
+        url=BASE_URL, json={'query': query}, headers=headers)
+    all_employees = response.json().get('data', {}).get(
+        'allEmployees', {}).get('edges', [])
     return all_employees
+
 
 def write_message(survey_reminder_message, channel):
     "Send a message to Skype Sender"
     sqs = boto3.client('sqs')
-    message = str({'msg':f'{survey_reminder}', 'channel':channel})
+    message = str({'msg': f'{survey_reminder_message}', 'channel': channel})
     sqs.send_message(QueueUrl=QUEUE_URL, MessageBody=(message))
+
 
 def survey_reminder():
     emp_data = get_all_employees()
     if date.today().weekday() == 3:
         for each_node in emp_data:
-            msg = f"Hey {each_node['node']['firstname']} ,it's time to take the help survey!!"
-            write_message(msg,each_node['node']['skypeId'])
+            msg = f"Hey {each_node['node']['firstname']} ,it's time to take the Help Survey: {os.environ.get('HELP_SURVEY_URL')} "
+            write_message(msg, '8:'+each_node['node']['skypeId'])
     elif date.today().weekday() == 4:
-        non_responded_users = requests.get(NOT_RESPONDED_USERS_URL,headers={'Accept':'application/json','User':os.environ.get('AUTHORISED_USER')})
-        msg = 'Non responded message'
-    return msg
+        non_responded_users = requests.get(NOT_RESPONDED_USERS_URL, headers={
+                                           'Accept': 'application/json', 'User': os.environ.get('AUTHORISED_USER')})
+        users = [user['email'] for user in non_responded_users.json()]
+        user_names = [user['fullName'] for user in non_responded_users.json()]
+
+        if (len(users) == 0):
+            msg = "Thank you all for submitting help survey on time!!"
+            write_message(msg, os.environ.get('CHANNEL_ID'))
+        else:
+            for each_node in emp_data:
+                if each_node['node']['email'] in users:
+                    msg = f"Hey {each_node['node']['firstname']},\nReminder to take Help Survey: {os.environ.get('HELP_SURVEY_URL')}"
+                    write_message(msg, '8:'+each_node['node']['skypeId'])
+            group_msg = "List of People who have not submitted help survey \n " + \
+                "\n" .join(user_names)
+            write_message(group_msg, os.environ.get('CHANNEL_ID'))
+
 
 def lambda_handler(event, context):
     "lambda entry point"
     message = survey_reminder()
-
-    
