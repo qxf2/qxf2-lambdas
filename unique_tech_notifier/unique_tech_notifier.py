013d915a4d5d7fc164dc098bc2d8f947626f9586
Rohan Joshi <rohan.j@qxf2.com>
2021-06-22 10:16:07 +0530

    Updated code to fetch tech per week from an endpoint

--- unique_tech_notifier/unique_tech_notifier.py
+++ unique_tech_notifier/unique_tech_notifier.py
@@ -5,10 +5,13 @@ import os
 import json
 import requests
 import boto3
+from datetime import date
 
 QUEUE_URL = 'https://sqs.ap-south-1.amazonaws.com/285993504765/skype-sender'
 ALL_TECH_URL = os.environ.get('ALL_TECH_URL')
 WEEKLY_TECH_URL = os.environ.get('WEEKLY_TECH_URL')
+AUTHORIZED_USER = os.environ.get('AUTHORIZED_USER')
+
 
 def write_message(unique_tech_msg, channel):
     "Send a message to Skype Sender"
@@ -16,16 +19,21 @@ def write_message(unique_tech_msg, channel):
     message = str({'msg':f'{unique_tech_msg}', 'channel':channel})
     sqs.send_message(QueueUrl=QUEUE_URL, MessageBody=(message))
 
+
 def get_all_techs():
     "Returns a list of technology data"
-    all_tech = requests.get(ALL_TECH_URL,headers={"Accept":"application/json","User":"qxf2"})
+    all_tech = requests.get(ALL_TECH_URL,headers={"Accept":"application/json","User":AUTHORIZED_USER})
     tech_list = [tech['technology'] for tech in all_tech.json()]
     return tech_list
 
+
 def get_weekly_tech():
     "Returns a list of technlogy data for current week"
-    weekly_tech = ['python converter','rust','BDD-PYTHON','FREE MIND']
-    return weekly_tech
+    todays_date = str(date.today())
+    weekly_tech = requests.post(WEEKLY_TECH_URL,data=json.dumps({"date":todays_date}),headers={"Accept":"application/json","Content-type":"application/json","User":AUTHORIZED_USER})
+    weekly_tech_list = [tech['Technology'] for tech in weekly_tech.json()]
+    return weekly_tech_list
+
 
 def get_unique_tech():
     "Retun weekly unique tech"
