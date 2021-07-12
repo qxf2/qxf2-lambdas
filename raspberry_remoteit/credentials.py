e34e4010ebb6ef924cb162f64a21fa3fc1838b93
Sravanti <sravanti.tatiraju@qxf2.com>
2021-07-12 11:50:10 +0530

    Raspberry remoteit integration

---
+++ raspberry_remoteit/credentials.py
@@ -0,0 +1,12 @@
+username = "rohan@qxf2.com"
+password = "TestQxf2"
+proxy_username = "pi"
+proxy_password = "raspberry"
+developerkey = "NzE5Rjk3OUYtNzNGQS00NjU5LUFBNzAtOUYyQjFDNkNEODA3"
+
+# import os
+# USERNAME = os.environ['USERNAME']
+# PASSWORD = os.environ['PASSWORD']
+# PROXY_USERNAME = os.environ['PROXY_USERNAME']
+# PROXY_PASSWORD = os.environ['PROXY_PASSWORD']
+# DEVELOPER_KEY = os.environ['DEVELOPER_KEY']
