a7ae4dcb8f2cb62d554482c52865858db0a10e00
Rahul.Bhave <rahul.bhave@qxf2.com>
2021-07-20 16:58:44 +0530

    added unit test using mock and renamed other code level test cases
    removed unit word from the test docstrings
    
    updated test docstring to update word tests
    
    updated docstring in the class
    
    changed the docstring indicating unit test
    
    pylint issue fix
    
    added assert called with url and data

--- tests/test_pto_detector_get_is_pto.py
+++ tests/test_pto_detector_get_is_pto.py
@@ -1,5 +1,5 @@
 """
-Unit tests for get_is_pto method of pto_detector lambda
+Code level tests for get_is_pto method of pto_detector lambda
 """
 import requests
 from parameterized import parameterized_class
@@ -13,10 +13,9 @@ class Testgetispto(object):
     """
     Test class for get is pto method
     """
-    #@responses.activate
-    def test_unit_get_is_pto_score(self):
+    def test_get_is_pto_score(self):
         """
-        Unit test for pto_score
+        code level test for pto_score
         """
         resp = requests.post(self.url, data={"message":self.message})
         assert resp.status_code == self.expected_status_code
