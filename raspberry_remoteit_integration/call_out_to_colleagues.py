e9db2f582c0a650c09b9946d9bc76a61504e64df
Sravanti <sravanti.tatiraju@qxf2.com>
2021-07-26 11:48:58 +0530

    Updated espeak command to use different voice

--- raspberry_remoteit_integration/call_out_to_colleagues.py
+++ raspberry_remoteit_integration/call_out_to_colleagues.py
@@ -1,2 +1,9 @@
+"""
+Module to play voice on raspberry pi
+"""
 import os
-os.system("espeak -s 125 --stdout 'Hi, one of our colleague is on the Jitsi call. Please join if you are free and want to have a quick chat' | aplay")
+
+os.system(
+    "espeak -ven-us+f3 -s 125 --stdout 'Hi, one of our colleague is on the Jitsi call. \
+    Please join if you are free and want to have a quick chat' | aplay 2>/dev/null"
+)
