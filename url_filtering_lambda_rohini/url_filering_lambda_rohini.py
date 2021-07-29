55b4cdd41734db82ee9daaa59d69741828575eac
Rohini Gopal <rohini.gopal@qxf2.com>
2021-07-29 14:01:28 +0530

    added url filtering code

--- url_filtering_lambda_rohini/url_filering_lambda_rohini.py
+++ url_filtering_lambda_rohini/url_filering_lambda_rohini.py
@@ -3,10 +3,36 @@ Simplest lambda to play with GitHub actions
 """
 import json
 import boto3
+import re
+
+
+def get_message_contents(event):
+    "Retrieve the message contents from the SQS event"
+    record = event.get('Records')[0]
+    message = record.get('body')
+    message = json.loads(message)['Message']
+    message = json.loads(message)
+
+    return message
+
+def get_url(message):
+    message = "https://www.geeksforgeeks.org/python-check-url-string/"
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex,message)      
+    return [x[0] for x in url]
+    
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    content = get_message_contents(event)
+    message = content['msg']
+    channel = content['chat_id']
+    user = content['user_id']
+    print(f'{message}, {user}, {channel}')
+    
+    if channel == os.environ.get('ETC_CHANNEL') and user != os.environ.get('Qxf2Bot_USER')::
+        url = get_url(message)
+        if url:
+            print("Call the API")
+        else:
+            print("No API found")
+
