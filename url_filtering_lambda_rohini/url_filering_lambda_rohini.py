427d94881c0f5512ce9c72e2d99b55b9031770ff
Rohini Gopal <rohini.gopal@qxf2.com>
2021-07-30 12:39:40 +0530

    cleaned the code

--- url_filtering_lambda_rohini/url_filering_lambda_rohini.py
+++ url_filtering_lambda_rohini/url_filering_lambda_rohini.py
@@ -1,5 +1,5 @@
 """
-Simplest lambda to play with GitHub actions
+Lambda to to pull URL from ETC channel messages
 """
 import json
 import os
@@ -7,6 +7,12 @@ import boto3
 import requests
 import re
 
+def clean_message(message):
+    "Clean up the message received"
+    message = message.replace("'", '-')
+    message = message.replace('"', '-')
+
+    return message
 
 def get_message_contents(event):
     "Retrieve the message contents from the SQS event"
@@ -14,20 +20,14 @@ def get_message_contents(event):
     message = record.get('body')
     message = json.loads(message)['Message']
     message = json.loads(message)
-
+    
     return message
 
 def get_url(message):
-    message = "https://www.geeksforgeeks.org/python-check-url-string/"
+    "Get the URL from the message"
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(regex,message)      
     return [x[0] for x in url]
-
-def get_url_from_message(message):
-    print("Inside get_url_from_message ")
-    if "href" in message:
-        print(message)
-    
 
 def lambda_handler(event, context):
     content = get_message_contents(event)
@@ -36,11 +36,17 @@ def lambda_handler(event, context):
     user = content['user_id']
     print(f'{message}, {user}, {channel}')
     
+    final_url=""
     if channel == os.environ.get('ETC_CHANNEL') and user != os.environ.get('Qxf2Bot_USER'):
-        url = get_url(message)
-        if url:
-            print("Call the API")
-        else:
-            print("No API found")
-        print("Somthing happened!!!")
-        href_url = get_url_from_message(message)
+        print("Getting message posted on ETC ")
+        cleaned_message = clean_message(message)
+        final_url=get_url(cleaned_message)
+        print("Final url is :",final_url)
+    else:
+        print("NO URL in ETC channel")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(final_url)
+    }
+    
