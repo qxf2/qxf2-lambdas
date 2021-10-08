8f9dbae9e9a643c61331a7aeb309ab5fd9d22e70
nilaya1234 <nilaya@qxf2.com>
2021-10-08 13:04:28 +0530

    added contract test for pto detector

---
+++ tests/test_contract_pto.py
@@ -0,0 +1,29 @@
+import atexit
+import unittest
+import requests
+import json
+from pact import Consumer, Provider,Like
+from pact.matchers import get_generated_values
+
+pact = Consumer('pto-detector-lambda').has_pact_with(Provider('pto-detector-instance'), host_name='localhost', port=1234)
+pact.start_service()
+atexit.register(pact.stop_service)
+
+
+class ContractTest(unittest.TestCase):
+  def test_is_pto(self):
+    expected = {"message":"I am doing fine today", "score": 0}
+    payload = {"message":"I am doing fine today"}
+
+    (pact
+     .given('a correct pto message')
+     .upon_receiving('to check if its a pto')
+     .with_request('post', '/is-pto',body=payload, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+     .will_respond_with(200, body=expected))
+
+    with pact:
+      result = requests.post('http://localhost:1234/is-pto',data=payload,headers={'Content-Type': 'application/x-www-form-urlencoded'})
+    self.assertEqual(result.json(), get_generated_values(expected))
+
+if __name__ == "__main__":
+     unittest.main()
