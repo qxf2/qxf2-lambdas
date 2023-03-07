2c2c6eae02cc7c1a2ea836e701688c3ba81755a7
Sravanti <sravanti.tatiraju@qxf2.com>
2023-03-07 12:55:59 +0530

    Updated message in body

--- dummy_rust_lambda/src/main.rs
+++ dummy_rust_lambda/src/main.rs
@@ -12,7 +12,7 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     let resp = Response::builder()
         .status(200)
         .header("content-type", "text/html")
-        .body("Hello AWS Lambda Dummy Rust Lambda".into())
+        .body("Hello, this is AWS Lambda Dummy Rust Lambda".into())
         .map_err(Box::new)?;
     Ok(resp)
 }
