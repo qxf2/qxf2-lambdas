f4324d0442e3408a7ae94af90a3958519d72669f
Sravanti <sravanti.tatiraju@qxf2.com>
2023-03-09 13:42:03 +0530

    multiple lambdas test

--- example_rust_lambdas/rust_lambda1/src/main.rs
+++ example_rust_lambdas/rust_lambda1/src/main.rs
@@ -4,7 +4,7 @@ use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
     // Extract some useful information from the request
 
     // Return something that implements IntoResponse.
@@ -12,7 +12,7 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     let resp = Response::builder()
         .status(200)
         .header("content-type", "text/html")
-        .body("Test example rust lambda with dot".into())
+        .body("Hello AWS Lambda HTTP request".into())
         .map_err(Box::new)?;
     Ok(resp)
 }
