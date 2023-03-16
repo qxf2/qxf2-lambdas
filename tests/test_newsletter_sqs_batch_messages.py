391a4a23db51b300d1e5332c74ce5500e3639381
akkuldn <akkul.dn@qxf2.com>
2023-03-16 15:16:23 +0530

    Added test to fetch articles from newsletter app and verify if all the articles sent are stored in the newsletter app

--- tests/test_newsletter_sqs_batch_messages.py
+++ tests/test_newsletter_sqs_batch_messages.py
@@ -6,43 +6,83 @@ The test covers the following scenarios:
 2. Recieve messages from the SQS and make sure all the messages sent earlier are present in the retrieved messages
 """
 import os
-import boto3
 import time
-import unittest
+import requests
 import aws_sqs_batchlib
+import json
+import pandas as pd
+import pytest
+import concurrent.futures
 
-class TestSQS(unittest.TestCase):
+queue_url = os.environ.get('newsletter_queue_url')
+api_key = os.environ.get('api_key')
+newsletter_automation_url = os.environ.get('newsletter_automation_url')
+messages = []
+articles_sent = []
 
-    @classmethod
-    def setUpClass(cls):
-        cls.queue_url = os.environ.get('newsletter_queue_url')
-        cls.messages = []
+def send_batch_messages():
+    "Send batch messages to the SQS and verify if all messages are sent"
 
-    def test_send_batch_messages(self):
-        "Send batch messages to the SQS and verify if all messages are sent"
+    # Create a list of messages to send
+    message_count=50
+    for message in range(message_count):
+        url = f"https://stage-newsletter-lambda-test-{message}.com"
+        articles_sent.append(url)
+        message_body = {"Message" : "{\"user_id\": \".cid.f9000d4f3453e374\", \"chat_id\": \"19:1941d15dada14943b5d742f2acdb99aa@thread.skype\", \"msg\": \"" + url + "\"}"}
+        messages.append({"Id": f'{message}', "MessageBody": json.dumps(message_body)})
 
-        # Create a list of messages to send
-        message_count=50       
-        for message in range(message_count):
-            self.messages.append({'Id': f'{message}', 'MessageBody': f'https://www.test-qxf2.com/{message}'})
-        
-        # Send the messages to the queue
-        res = aws_sqs_batchlib.send_message_batch(
-            QueueUrl=self.queue_url,
-            Entries=self.messages,
-        )
-        assert len(res['Successful']) == message_count        
+    time.sleep(3)
+    # Send the messages to the queue
+    response = aws_sqs_batchlib.send_message_batch(
+        QueueUrl=queue_url,
+        Entries=messages,
+    )
+    return len(response['Successful']) == message_count
 
-    def test_receive_messages(self):
-        "Retreive messages from the SQS and verify if all the messages sent are retrieved"
-        # Receive the messages from the queue
+def receive_messages():
+    "Retreive messages from the SQS and verify if all the messages sent are retrieved"
+    # Receive the messages from the queue
+    start_time = time.time()
+    max_wait_time = 180
+    while True:
         res = aws_sqs_batchlib.receive_message(
-            QueueUrl = self.queue_url,
+            QueueUrl = queue_url,
             MaxNumberOfMessages=100,
-            WaitTimeSeconds=15,
+            WaitTimeSeconds=20,
         )
-               
+        received_articles = []
         # Verify that the messages match the sent messages
         received_messages = {msg['Body'] for msg in res['Messages']}
-        for message in self.messages:
-            self.assertIn(message['MessageBody'], received_messages, f"Message {message['MessageBody']} should be received")
+        
+        for message in received_messages:
+            json_message = json.loads(message)
+            recieved_msg_body = json.loads(json_message['Message'])
+            received_articles.append(recieved_msg_body['msg'])
+
+        elapsed_time = time.time() - start_time
+        if len(received_articles) == 0 or elapsed_time >= max_wait_time:
+            break
+    return "No more messages in queue"
+
+def test_send_and_receive_batch_messages(printer):
+    "Send and receive batch messages to the SQS"
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        execute_receive_messages = executor.submit(receive_messages)
+        execute_send_messages = executor.submit(send_batch_messages)      
+        return_value_send_messages = execute_send_messages.result()
+        return_value_recieve_messages = execute_receive_messages.result()
+        printer(return_value_recieve_messages)
+
+    assert return_value_send_messages is True,f"Not all messages were sent successfully"
+    
+def test_if_articles_added():
+    "Test if articles are added to the database"
+
+    staging_newsletter_url = newsletter_automation_url if newsletter_automation_url is not None else "https://staging-newsletter-generator.qxf2.com/api/articles/all"
+    headers = {'x-api-key' : api_key}
+    response = requests.get(staging_newsletter_url, headers=headers).text
+    articles = pd.read_json(response)
+    article_url = articles['url'].tolist()
+    assert all(item in article_url for item in articles_sent),f"Not all articles sent were added into the database"
+
