4e077de9afa59edb99779071b76fec4dfd97c71d
Archana GS <gsarchana93@gmail.com>
2023-07-10 12:28:43 +0530

    resolving issues while running script using pylint

--- unique_tech_notifier/unique_tech_notifier.py
+++ unique_tech_notifier/unique_tech_notifier.py
@@ -1,11 +1,12 @@
 """
-This lambda will notify on skype channel about the unique tech learnt for the current week based on the survey data
+This lambda will notify on skype channel about the
+unique tech learnt for the current week based on the survey data
 """
+from datetime import date, timedelta
 import os
 import json
 import requests
 import boto3
-from datetime import date,timedelta
 
 QUEUE_URL = 'https://sqs.ap-south-1.amazonaws.com/285993504765/skype-sender'
 ALL_TECH_URL = os.environ.get('ALL_TECH_URL')
@@ -23,7 +24,10 @@ def write_message(unique_tech_msg, channel):
 def get_all_techs():
     "Returns a list of technology data"
     end_date = str(date.today() - timedelta(days=5))
-    all_tech = requests.post(ALL_TECH_URL,headers={"Accept":"application/json","User":AUTHORIZED_USER},data=json.dumps({"start_date":"2014-01-01","end_date":end_date}))
+    all_tech = requests.post(
+        ALL_TECH_URL,
+        headers={"Accept":"application/json", "User":AUTHORIZED_USER},
+        data=json.dumps({"start_date":"2014-01-01", "end_date":end_date}))
     tech_list = [tech['technology'] for tech in all_tech.json()]
     return tech_list
 
@@ -31,15 +35,20 @@ def get_all_techs():
 def get_weekly_tech():
     "Returns a list of technlogy data for current week"
     todays_date = str(date.today())
-    weekly_tech = requests.post(WEEKLY_TECH_URL,data=json.dumps({"date":todays_date}),headers={"Accept":"application/json","Content-type":"application/json","User":AUTHORIZED_USER})
+    weekly_tech = requests.post(
+        WEEKLY_TECH_URL,
+        data=json.dumps({"date":todays_date}),
+        headers={
+            "Accept":"application/json",
+            "Content-type":"application/json",
+            "User":AUTHORIZED_USER})
     weekly_tech_list = [tech['Technology'] for tech in weekly_tech.json()]
     return weekly_tech_list
 
 
 def get_unique_tech():
     "Retun weekly unique tech"
-    unique_tech_list =set(get_weekly_tech()) - set(get_all_techs())
-    
+    unique_tech_list = set(get_weekly_tech()) - set(get_all_techs())
     if len(unique_tech_list) != 0:
         msg = "List of unique techs learnt this week:\n"+"\n".join(unique_tech_list)
     else:
@@ -50,10 +59,10 @@ def get_unique_tech():
 def lambda_handler(event, context):
     "lambda entry point"
     message = get_unique_tech()
-    write_message(message, event.get('channel','main'))
+    write_message(message, event.get('channel', 'main'))
     weekly_tech_list = set(get_weekly_tech())
     if len(weekly_tech_list) != 0:
         msg = "List of techs reported this week:\n"+"\n".join(weekly_tech_list)
     else:
         msg = "*No techs* reported this week!! :("
-    write_message(msg, event.get('channel','main'))
+    write_message(msg, event.get('channel', 'main'))
